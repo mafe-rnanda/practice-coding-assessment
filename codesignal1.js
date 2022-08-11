ac9eb83168935b73ebfd1d993af8844a9ef02e60
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2022-08-11 10:34:11 -0700

    first commit

---
+++ codesignal1.js
@@ -0,0 +1,33 @@
+// Let's say a triple (a, b, c) is a zigzag if either a < b > c or a > b < c.
+
+// Given an array of integers numbers, your task is to check all the triples of its consecutive elements for being a zigzag. More formally, your task is to construct an array of length numbers.length - 2, where the ith element of the output array equals 1 if the triple (numbers[i], numbers[i + 1], numbers[i + 2]) is a zigzag, and 0 otherwise.
+
+// Example
+
+// For numbers = [1, 2, 1, 3, 4], the output should be solution(numbers) = [1, 1, 0].
+
+// (numbers[0], numbers[1], numbers[2]) = (1, 2, 1) is a zigzag, because 1 < 2 > 1;
+// (numbers[1], numbers[2] , numbers[3]) = (2, 1, 3) is a zigzag, because 2 > 1 < 3;
+// (numbers[2], numbers[3] , numbers[4]) = (1, 3, 4) is not a zigzag, because 1 < 3 < 4;
+// For numbers = [1, 2, 3, 4], the output should be solution(numbers) = [0, 0];
+
+function solution(numbers) {
+    let result = [];
+    let size = numbers.length;
+
+    for (let i = 0; i < size - 2 ; i++) {
+        if ((numbers[i] > numbers[i + 1] && numbers[i + 1] < numbers[i + 2]) || (numbers[i] < numbers[i + 1] && numbers[i + 1] > numbers[i + 2])){
+            result.push(1)
+        } else {
+            result.push(0)
+        }
+    }
+    return result;
+}
+
+// Tests
+// console.log(solution([1, 2, 3, 4]));
+// console.log(solution([1, 1, 1]));
+// console.log(solution([10, 11, 13, 12, 10, 9]));
+// console.log(solution([10, 11, 13, 12, 10, 9, 5, 6, 7, 10]));
+
